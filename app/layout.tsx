f7f9875d2157da5ba4bffe2c27dc3aee8ed98949
astrit <am@astrit.co>
2025-09-10 15:27:53 +0200

    Msg → Wed Sep 10 15:27:53 CEST 2025

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,11 @@
+import type { Metadata } from "next";
+
 import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Mobil miljöstation",
+  description: "Built by Astrit",
+};
 
 export default function RootLayout({
   children,
